61488fa0d487615a2567633c571b28caa762582c
krishkansal7 <krish1412.be20@chitkara.edu.in>
2022-08-03 18:37:22 +0530

    commit2

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn=(document.querySelector('.btn-size'))
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()     
+const textvalue1=document.querySelector('.inpt1').value
+console.log(textvalue1);
+
+const textvalue2=document.querySelector('.inpt2').value
+console.log(textvalue2);
+
+const textvalue3=document.querySelector('.inpt3').value
+console.log(textvalue3);
+
+const textvalue4=document.querySelector('.inpt4').value
+console.log(textvalue4);
+
+const textvalue5=document.querySelector('.message').value
+console.log(textvalue5);
+})
